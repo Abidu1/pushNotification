b787374ece8e832e5eedf10bb24456749b4264ce
Abidu1 <abidusra@gmail.com>
2021-07-16 04:41:40 -0400

    testing

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
     function notifyMe() {
         // Let's check if the browser supports notifications
         if (!("Notification" in window)) {
-          alert("This browser does not support desktop notification");
-        }
+          window.onload=function(){
+            var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+            if (mobile) {
+                alert("Visit this on a Computer for Better View");              
+            }}}
       
         // Let's check whether notification permissions have already been granted
         else if (Notification.permission === "granted") {
