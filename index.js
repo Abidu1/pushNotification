9592af973991c3a721c7514e5dde1f7abf7b4bf6
Abidu1 <abidusra@gmail.com>
2021-07-18 21:22:17 -0400

    testing

--- index.js
+++
@@ -1,9 +0,0 @@
-if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("/pushNotification/sw.js").then(registration =>{
-        console.log("SW Registered");
-        console.log(registration)
-    }).catch(error =>{
-        console.log('sw Registration failed');
-        console.log(error)
-    })
-}
